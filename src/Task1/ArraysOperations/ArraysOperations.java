59da9fffa53674d289299ecac12883a018aad0aa
ivankosenko <kosenko.bmstu@gmail.com>
2022-03-23 13:24:30 +1100

    first

--- src/Task1/ArraysOperations/ArraysOperations.java
+++ src/Task1/ArraysOperations/ArraysOperations.java
@@ -11,23 +11,41 @@ public class ArraysOperations {
     int SumArray[][];
 
     public static void main(String[] args) throws IOException {
-//        System.out.print("Введите первый размер первого массива ");
+
        var in = new Scanner(System.in);
-//        int size = in.nextInt();
-//        System.out.print("Введите второй размер первого массива ");
-//        int size2 = in.nextInt();
-//        System.out.printf("Размер первого массива %d на %d", size, size2);
+        //матрица размеров массивов
         int size[][] = new int[2][2];
+        //ввод размеров массивов
         for (int i =0; i<=1; i++){
             for(int j= 0; j<=1; j++){
                 System.out.printf("Введите %d размер %d массива ", j+1, i+1);
                 size[i][j] = in.nextInt();
             }
         }
+        //вывод размеров массивов
         System.out.println("Размеры массива");
         for (int[] a: size){
             System.out.println(Arrays.toString(a));
         }
+        //инициализация массивов
+        int[][] array1 = new int[size[0][0]][size[0][1]];
+        int[][] array2 = new int[size[1][0]][size[1][1]];
+        //ввод элементов массивов
+        for (int matrix =0; matrix <= 1; matrix++){
+            System.out.printf("Введите элементы %d массива ", matrix + 1);
+            for (int i = 0; i <= size[matrix][0]-1; i++) {
+                for (int j = 0; j <= size[matrix][1]-1; j++) {
+                    if (matrix == 0) array1[i][j] = in.nextInt();
+                    else if (matrix == 1) array2[i][j] = in.nextInt();
+                }
+            }
+        }
+        //вывод элементов массивов
+        System.out.println("Массивы");
+        for(int[] b : array1)
+            System.out.println(Arrays.toString(b));
         System.out.println();
+        for(int[] b : array2)
+            System.out.println(Arrays.toString(b));
     }
 }
